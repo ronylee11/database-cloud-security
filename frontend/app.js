f91e7cb8a7756890d187adb1857638df5a30bf90
Rony Lee <rongyil33@gmail.com>
2024-12-05 13:13:10 +0800

    feat: check api health

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,31 @@
-alert("hello world");
+//alert("hello world");
+
+const backendPort = 3001;
+const backendUrl = `http://localhost:${backendPort}`;
+
+// test backend api
+// @param {string} backendUrl
+// @returns JSON object { status: 'UP' } or { status: 'DOWN' }
+async function checkHealth(backendUrl) {
+  let healthStatus;
+  try {
+    const response = await fetch(`${backendUrl}/health`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    healthStatus = await response.json();
+    console.log(healthStatus);
+  } catch (error) {
+    healthStatus = { status: "DOWN" };
+    console.log(error);
+  }
+  return healthStatus;
+}
+
+//alert(checkHealth(backendUrl));
+checkHealth(backendUrl).then((healthStatus) => {
+  alert(healthStatus.status);
+});
